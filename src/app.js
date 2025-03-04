5cd3a38812255591b298033249a28b20c8f6bab4
Kachulina Tatyana <kachulinatanya@yandex.ru>
2025-03-04 11:52:02 +0300

    add input validation

--- src/app.js
+++ src/app.js
@@ -1,1 +1,58 @@
-export default () => {};
+import { string } from 'yup';
+import i18next from 'i18next';
+import watcher from './watcher.js';
+import resources from './locales/index.js';
+
+const validateUrl = async (url, existingUrls) => {
+  const schema = string().url('invalidUrl').required().notOneOf(existingUrls, 'repeatedUrl');
+  return schema.validate(url);
+};
+
+export default () => {
+  const elements = {
+    rssForm: document.querySelector('.rss-form'),
+    urlInput: document.querySelector('#url-input'),
+    feedbackMessage: document.querySelector('.feedback'),
+    submitBtn: document.querySelector('.rss-form [type="submit"]'),
+  };
+
+  const state = {
+    rssForm: {
+      state: 'filling',
+      error: null,
+    },
+    posts: [],
+    feeds: [],
+    uiState: {
+      links: new Set([]),
+    },
+  };
+
+  const i18nextInstance = i18next.createInstance();
+  i18nextInstance.init({
+    lng: 'ru',
+    debug: true,
+    resources,
+  }).then(() => {
+    const watchedState = watcher(state, elements, i18nextInstance);
+
+    elements.rssForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const formData = new FormData(elements.rssForm);
+      const input = formData.get('url');
+      validateUrl(input, state.uiState.links)
+        .then((url) => {
+          watchedState.rssForm.state = 'sending';
+          // сделать запрос по ссылке и обработать
+          // если успешно
+          watchedState.uiState.links.add(url);
+          watchedState.rssForm.state = 'idle';
+          watchedState.rssForm.error = null;
+        })
+        .catch((err) => {
+          watchedState.rssForm.state = 'invalid';
+          watchedState.rssForm.error = err.message;
+        });
+    });
+  });
+};
---
+++ src/locales/ru.js
@@ -0,0 +1,11 @@
+export default {
+  translation: {
+    success: 'RSS успешно загружен',
+    errors: {
+      invalidUrl: 'Ссылка должна быть валидным URL',
+      repeatedUrl: 'RSS уже существует',
+      noRss: 'Ресурс не содержит валидный RSS',
+      networkError: 'Ошибка сети',
+    },
+  },
+};
